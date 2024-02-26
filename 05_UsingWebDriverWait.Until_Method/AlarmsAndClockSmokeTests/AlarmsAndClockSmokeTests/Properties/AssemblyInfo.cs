e63e3ef810e664aa839f48bde401896263f1ed44
evandrolucas60 <evandrolucas60@yahoo.com>
2024-02-26 14:34:46 -0300

    Using WebDriverWait .Until Method to Wait for UI Controls in C# Test Automation

---
+++ 05_UsingWebDriverWait.Until_Method/AlarmsAndClockSmokeTests/AlarmsAndClockSmokeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AlarmsAndClockSmokeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlarmsAndClockSmokeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4f937be-fc96-49f9-bc94-96aec70bacee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
