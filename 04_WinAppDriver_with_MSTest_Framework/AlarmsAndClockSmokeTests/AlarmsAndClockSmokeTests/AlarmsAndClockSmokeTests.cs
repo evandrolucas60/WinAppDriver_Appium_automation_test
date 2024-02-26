e799860769a0712f034d96ef126c131d3f51b89f
evandrolucas60 <evandrolucas60@yahoo.com>
2024-02-26 14:24:14 -0300

    How to click an item in a ListView with Appium WinAppDriver Click, add new clock

---
+++ 04_WinAppDriver_with_MSTest_Framework/AlarmsAndClockSmokeTests/AlarmsAndClockSmokeTests/AlarmsAndClockSmokeTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AlarmsAndClockSmokeTests
+{
+    [TestClass]
+    public class AlarmsAndClockSmokeTests
+    {
+
+        static WindowsDriver<WindowsElement> sessionAlarms;
+        [ClassInitialize]
+        public static void PrepareForTestingAlarms(TestContext testContext)
+        {
+            Debug.WriteLine("Hello ClassInitialize");
+            AppiumOptions appOptions = new AppiumOptions();
+            appOptions.AddAdditionalCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            appOptions.AddAdditionalCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            sessionAlarms = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+        }
+
+        [ClassCleanup]
+        public static void CleanupAfterAllAlarmsTests()
+        {
+            //Debug.WriteLine("Hello ClassCleanup");
+            //if (sessionAlarms != null)
+            //{
+            //    sessionAlarms.Quit();
+            //}
+        }
+
+        [TestMethod]
+        public void JustAnotherTest()
+        {
+            Debug.WriteLine("Hello another test.");
+        }
+
+        [TestMethod]
+        public void TestAlarmsAndClockIsLaunchingSuccessfully()
+        {
+            Assert.AreEqual("Relógio", sessionAlarms.Title, false, $"Actual title doesn't match expected title: {sessionAlarms.Title}");
+        }
+
+        [TestMethod]
+        public void VerifyNewClockCanBeAdded()
+        {
+            //click the top button in top section of the app
+            sessionAlarms.FindElementByAccessibilityId("ClockButton").Click();
+            sessionAlarms.FindElementByName("Adicionar nova cidade").Click();
+            Thread.Sleep(1000);
+            var txtLocation = sessionAlarms.FindElementByName("Inserir um local");
+            txtLocation.SendKeys("São Paulo");
+            txtLocation.SendKeys(Keys.ArrowDown);
+            txtLocation.SendKeys(Keys.Enter);
+            
+            sessionAlarms.FindElementByName("Adicionar").Click();
+        }
+    }
+}
